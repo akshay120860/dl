7946180c24704e080c73c4067caadeca36a918f4
akshay120860 <n120860@rguktn.ac.in>
2017-07-16 18:35:59 +0530

    Delete 1.sh

--- 1.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-sudo apt-get -y install python-pip
-sudo apt-get update
-sudo apt-get install build-essential checkinstall
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
