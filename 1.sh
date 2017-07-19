ded33974f78da7e02dc79f6bb9fd5226679aca8a
akshay120860 <n120860@rguktn.ac.in>
2017-07-19 14:31:50 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,6 +3,6 @@ sudo apt-get -y install python-pip
 sudo apt-get update
 sudo apt-get install build-essential checkinstall
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-sudo apt-get install libcupti-dev
-pip install tensorflow 
+#sudo apt-get install libcupti-dev
+#pip install tensorflow 
 pip install pandas 
